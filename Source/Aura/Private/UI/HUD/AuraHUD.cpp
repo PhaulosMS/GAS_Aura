8adb219fa353af6eef2201a0a0c3038efc88ae3d
Mitch <mitchy08@hotmail.co.uk>
2023-04-12 16:18:35 +0100

    Added delegate to update health and max health values

--- Source/Aura/Private/UI/HUD/AuraHUD.cpp
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -32,6 +32,8 @@ void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySyst
 
 	OverlayWidget->SetWidgetController(WidgetController);
 
+	WidgetController->BroadcastInitialValues();
+
 	Widget->AddToViewport();
 }
 
